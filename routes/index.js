89ad2ad3c8db3ac37092fc2a0a95cbdd4f43c828
Sunspawn <nikolay.babkin@gmail.com>
2021-05-03 20:21:51 +0300

    did a whole lot of nothing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {ensureAuthenticated} = require('../config/auth')
+const mongoose = require('mongoose');
+
+//User model
+const User = require('../models/User')
+
+
 //Login Page
 router.get('/',(req, res) => res.render('welcome'))
 
@@ -10,7 +16,40 @@ res.render('dashboard',{
     name : req.user.name
 }))
 
+//HR index
 router.get('/hrindex', (req, res) => res.render('./crud_hr/index'));
+
+// User actions
+
+// Profile page
+router.get('/profile', ensureAuthenticated, (req, res) =>
+res.render('profile', {
+    name: req.user.name,
+    email: req.user.email
+}));
+
+// Profile edit submit
+router.post('/profile',
+    (req, res) => {
+        const { name, email } = req.body;
+
+        let errors = [];
+
+        User.findOne({ email: email })
+        .then(user => {
+                if (user) {
+                    errors.push({ msg: 'A user with this email already exists' });
+                }
+            })
+        .catch(err => console.log(err));
+
+        if (errors.length > 0) {
+            res.render('profile', { errors, name, email });
+        } else {
+            User.findOneAndUpdate({ email: req.user.email }, { name: name, email: email}, {new: true});
+        }
+    }
+);
 
 module.exports = router
 
