2fe9cb585cc044f61554a2506607f9b8d30332a0
Daniel Volinsky <danivoli5@gmail.com>
2021-05-21 19:16:46 +0300

    add crud and hr login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {ensureAuthenticated} = require('../config/auth')
+
 //Login Page
 router.get('/',(req, res) => res.render('welcome'))
 
@@ -10,7 +11,12 @@ res.render('dashboard',{
     name : req.user.name
 }))
 
-router.get('/hrindex', (req, res) => res.render('./crud_hr/index'));
+//hr login
+router.get('/hrlogin1', (req, res) =>{res.render('./hrlogin')} );
+
+//crud
+router.get('/hrcontrol', (req, res) =>{res.render('./crud')} );
+
 
 module.exports = router
 
--- routes/users.js
+++ routes/users.js
@@ -92,6 +92,9 @@ router.post('/login', (req, res, next) => {
     req.flash('success_msg', 'You are logged out');
     res.redirect('/users/login');
   });
+
+
+
   
 
 module.exports = router
